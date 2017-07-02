d24041ceecf31fae1b2bcbd65d2f0b8c33382033
abhishek <abhigupta@ebay.com>
2017-07-02 16:11:17 -0700

    More code

--- FindMedianFromDataStream.java
+++ FindMedianFromDataStream.java
@@ -1,4 +1,31 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class FindMedianFromDataStream {
+
+	private Queue<Long> minHeap = new PriorityQueue<Long>();
+
+	private Queue<Long> maxHeap = new PriorityQueue<Long>(10, Collections.reverseOrder());
+
+	public void addNum(int a) {
+
+		maxHeap.add((long) a);
+
+		minHeap.add(maxHeap.poll());
+
+		if (maxHeap.size() < minHeap.size()) {
+
+			maxHeap.add(minHeap.poll());
+
+		}
+
+	}
+
+	public double findMedian() {
+
+		return (maxHeap.size() > minHeap.size()) ? maxHeap.peek() : (maxHeap.peek() + minHeap.peek()) / 2.0;
+
+	}
 
 }
---
+++ TargetSum.java
@@ -0,0 +1,82 @@
+import java.util.Map;
+
+/***
+ * 
+ *  494. Target Sum 
+ *  
+ *  https://leetcode.com/problems/target-sum/#/submissions/1
+ *  
+ *   You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+
+Find out how many ways to assign symbols to make sum of integers equal to target S.
+
+Example 1:
+
+Input: nums is [1, 1, 1, 1, 1], S is 3. 
+Output: 5
+Explanation: 
+
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+
+There are 5 ways to assign symbols to make the sum of nums be target 3.
+
+Note:
+
+    The length of the given array is positive and will not exceed 20.
+    The sum of elements in the given array will not exceed 1000.
+    Your output answer is guaranteed to be fitted in a 32-bit integer.
+
+ * @author abhigupta
+ *
+ */
+public class TargetSum {
+	
+    /*
+ public int findTargetSumWays(int[] nums, int S) {
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        return helper(nums, 0, 0, S, new HashMap<>());
+    }
+    */
+    private int helper(int[] nums, int index, int sum, int S, Map<String, Integer> map){
+        String encodeString = index + "->" + sum;
+        if (map.containsKey(encodeString)){
+            return map.get(encodeString);
+        }
+        if (index == nums.length){
+            if (sum == S){
+                return 1;
+            }else {
+                return 0;
+            }
+        }
+        int curNum = nums[index];
+        int add = helper(nums, index + 1, sum - curNum, S, map);
+        int minus = helper(nums, index + 1, sum + curNum, S, map);
+        map.put(encodeString, add + minus);
+        return add + minus;
+    }
+    
+     public int findTargetSumWays(int[] nums, int s) {
+        int sum = 0;
+        for (int n : nums)
+            sum += n;
+        return sum < s || (((s + sum) & 1) == 1)? 0 : subsetSum(nums, s + sum); 
+    }   
+
+    public int subsetSum(int[] nums, int s) {
+        s = s/2;
+        int[] dp = new int[s + 1]; 
+        dp[0] = 1;
+        for (int n : nums)
+            for (int i = s; i >= n; i--)
+                dp[i] += dp[i - n]; 
+        return dp[s];
+    } 
+
+}
